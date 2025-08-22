71f6ab4204d9957d96d560e6720154db013ef2ea
N1nt4nd0 <51034643+N1nt4nd0@users.noreply.github.com>
2025-08-22 12:28:09 +0300

    MapStruct implementation done

--- src/main/java/ru/feodorkek/dev/perfectcrud/mapper/TeamMapper.java
+++ src/main/java/ru/feodorkek/dev/perfectcrud/mapper/TeamMapper.java
@@ -1,9 +1,12 @@
 package ru.feodorkek.dev.perfectcrud.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import ru.feodorkek.dev.perfectcrud.dto.model.TeamDto;
 import ru.feodorkek.dev.perfectcrud.dto.response.TeamResponse;
 import ru.feodorkek.dev.perfectcrud.model.Team;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TeamMapper {
 
     TeamResponse toResponse(TeamDto dto);
--- src/main/java/ru/feodorkek/dev/perfectcrud/mapper/WorkspaceMapper.java
+++ src/main/java/ru/feodorkek/dev/perfectcrud/mapper/WorkspaceMapper.java
@@ -1,5 +1,8 @@
 package ru.feodorkek.dev.perfectcrud.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import ru.feodorkek.dev.perfectcrud.dto.model.TeamDto;
 import ru.feodorkek.dev.perfectcrud.dto.model.WorkspaceDto;
 import ru.feodorkek.dev.perfectcrud.dto.response.WorkspaceResponse;
@@ -7,8 +10,10 @@ import ru.feodorkek.dev.perfectcrud.model.Workspace;
 
 import java.util.List;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = TeamMapper.class)
 public interface WorkspaceMapper {
 
+    @Mapping(target = "teams", source = "teamDtoList")
     WorkspaceResponse toResponse(WorkspaceDto dto, List<TeamDto> teamDtoList);
 
     WorkspaceDto toDto(Workspace workspace);
