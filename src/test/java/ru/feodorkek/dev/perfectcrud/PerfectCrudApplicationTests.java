9d743ea91684668b58a3c58f7658af16e4e5d74c
N1nt4nd0 <51034643+N1nt4nd0@users.noreply.github.com>
2025-08-27 17:07:41 +0300

    TODO - implement some tests

--- src/test/java/ru/feodorkek/dev/perfectcrud/PerfectCrudApplicationTests.java
+++ src/test/java/ru/feodorkek/dev/perfectcrud/PerfectCrudApplicationTests.java
@@ -1,13 +1,12 @@
 package ru.feodorkek.dev.perfectcrud;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PerfectCrudApplicationTests {
 
     @Test
     void contextLoads() {
+        // TODO implement tests
     }
 
 }
